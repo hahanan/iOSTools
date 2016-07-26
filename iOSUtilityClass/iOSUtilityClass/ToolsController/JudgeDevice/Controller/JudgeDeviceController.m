32edf8b1c20d47b52e1dc39192aadd8666d06c90
hahanan <15801207364@163.com>
2016-07-26 22:41:12 +0800

    判断设备机型

--- iOSUtilityClass/iOSUtilityClass/ToolsController/JudgeDevice/Controller/JudgeDeviceController.m
+++ iOSUtilityClass/iOSUtilityClass/ToolsController/JudgeDevice/Controller/JudgeDeviceController.m
@@ -16,14 +16,26 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self prepareUI];
+   
+}
+
+- (void)prepareUI {
+
+    UILabel *label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:label];
+    
+    // 此方法只适用于真机使用,模拟器使用无效
     NSString *deviceString = [JudgeDeviceManager shareMatchManager].doDevicePlatform;
     if ([deviceString isEqualToString:@"iPhone 4S"] || [deviceString isEqualToString:@"iPhone 4"]) {
-       
+        
+        label.frame = CGRectMake(10, 100, 80, 30);
         NSLog(@"设备为iPhone 4 或者iPhone 4s");
     }else {
         
+        label.frame = CGRectMake(20, 100, 100, 30);
         NSLog(@"设备不是iPhone 4 或者iPhone 4s");
-
     }
 }
 @end
